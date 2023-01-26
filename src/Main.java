45429218740a73560934c5c8b05025f428240c89
User <asanbekovmalik2@gmail.com>
2023-01-26 14:04:35 +0600

    м

---
+++ src/Main.java
@@ -0,0 +1,233 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    /*    System.out.println(one(5,3,9));
+
+    }
+
+    static double one(int a,int s,int d) {
+      return (a+s+d)/3d;*/
+
+/*  //TODO  sabaktagy tapchyrma-3
+
+      System.out.println(names("abdumalik"));
+    }
+    static String names(String name){
+
+
+        if (name.length()<=10){
+            return "Атыныз узун";
+        } else if (name.length()<=6) {
+            return "атыныз орточо";
+        }else {
+            return "aтыныз кыска";
+        }*/
+
+/* //TODO 9-sabaktagy 4-tapshyrma
+
+ int []numbers={13,54,76,64,22,65,86,45,46,23,44};
+        System.out.println(number( numbers));
+
+
+    }
+
+
+    public static int number(int[] aray) {
+        Arrays.sort(aray);
+        return aray[10];*/
+        /*//TODO kaicy san chon je kichine
+
+        int[] numbers = {34, 14, 76, 22, 45, 37};
+        System.out.println(number(numbers));
+    }
+
+    static int number(int[] aray) {
+        int min = aray[1];
+        for (int i = 0; i < aray.length; i++) {
+            if (aray[i] > min) {
+                min=aray[i];
+            }
+
+        }
+        return min;*/
+ /*//TODO sabaktagy tapshyrma
+
+atym("mako:",19);
+        System.out.println(menin("Malik:",18));
+         }
+    static String menin(String name,int age){
+return name+age;
+    }
+static void atym(String name,int age){
+    System.out.println(name+age);
+*/
+       /*  //TODO sabaktagy tapshyrma
+
+        int[] num = new int[20];
+        for (int i = 0; i < num.length; i++) {
+            num[i] = san(1, 20);
+            System.out.print(num[i] + " ");
+
+        }
+
+    }
+static int san(int a,int b){
+        Random random=new Random();
+        int s= random.nextInt(a,b);
+   return s;
+        misal(0);
+}
+static void misal(int number) {
+    Scanner scanner = new Scanner(System.in);
+    int array = 0;
+    int san = 0;
+    number = scanner.nextInt();
+    while (number >= 1) ;
+    array = array % 10;
+    san = san * 10 + array;
+    number = number / 10;
+
+    System.out.println(san);
+    System.out.println(san + 8);
+*/
+    /*  //TODO lestnisa
+      for (int i = 0; i <5 ; i++) {
+            for (int j = 0; j < 5-i; j++) {
+                System.out.print(" ");
+
+            }
+            if (i==0){
+                System.out.print("@");
+            }
+            for (int j = 0; j <1+i/5; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }*/
+   /*  //TODO sabaktagy misal
+
+      System.out.println(myInt());
+    }
+
+    static int myInt() {
+        int summa=0;
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0 && i % 4 == 0 && i % 5 == 0) {
+                summa=i;
+            }
+        }
+        return summa;*/
+   /*   //TODO JASYNDU TAP
+
+     Scanner scanner=new Scanner(System.in);
+        int menin = scanner.nextInt();
+        System.out.println(meninJashum(menin));
+    }
+static int meninJashum(int age){
+        return age-2004;
+}
+
+    }*/
+/* //TODO HOM WORK
+    Scanner scanner=new Scanner(System.in);
+    int d= scanner.nextInt();
+        System.out.println(method(d,1,2,3,4,5));
+
+    }
+
+    static boolean method(int a,int... varas) {
+        for (int f = 0; f < varas.length ; f++) {
+           if (a==varas[f]){
+            return true;
+           }
+        }
+        return false;*/
+    /*//TODO nedeila s metodom
+
+       Scanner scanner=new Scanner(System.in);
+        int kun= scanner.nextInt();
+        System.out.println(nedelia(kun));
+    }
+
+    static String nedelia(int den) {
+        return switch (den) {
+            case 1 -> " ponedelnik";
+            case 2 -> "vtornik";
+            case 3 -> "sreda";
+            case 4 -> "chetverg";
+            case 5 -> "piatnitsa";
+            case 6 -> "subota";
+            case 7 -> "voskrisenia";
+            default -> null;
+        };*/
+/*        //TODO Bytpoi kalgan tapshyrma
+
+       int[] f = {1, 2, 3, 4, 4,5,5, 6};
+        System.out.println(work(f));
+
+    }
+
+    static boolean work(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+
+
+
+
+            return true;
+
+    }
+        return false;*/
+}
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*static void min(int[] aray){
+        Arrays.sort(aray);
+    System.out.println("minumum"+aray[0]);
+}
+
+static void max(int[]aray){
+
+    Arrays.sort(aray);
+        System.out.println("maximum"+aray[11]);
+
+
+}*/
+
+
